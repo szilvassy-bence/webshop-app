d837690b57fc0d14b8d8f81778c0a1985b4b208f
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-08 19:53:10 +0200

    integrated admin-services into admin dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -14,4 +14,12 @@
             </div>
         </div>
     </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Services') }}
+            </h2>
+            <x-admin-services />
+        </div>
+    </div>
 </x-admin-layout>
