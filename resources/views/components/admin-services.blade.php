335f37034194a1a09962b47ab86b8dbcd76d4af4
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-08 19:52:39 +0200

    created a basic card for displaying and admin service and a services component to hold all services

---
+++ resources/views/components/admin-services.blade.php
@@ -0,0 +1,5 @@
+<div class="grid grid-cols-4 gap-4 mt-6">
+    <h1 class="text-red-500">Test Heading</h1>
+    <x-admin-card route="admin.users.index">Users</x-admin-card>
+{{--    <x-admin-card route="admin.products.index">Products</x-admin-card>--}}
+</div>
