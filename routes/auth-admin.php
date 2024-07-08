0970f63071e1d6fac8e65daf45ad7aaff30adfd7
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-08 13:11:03 +0200

    creating endpoints for logging in and logging out

--- routes/auth-admin.php
+++ routes/auth-admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthenticatedAdminSessionController;
 use App\Http\Controllers\Admin\RegisteredAdminController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,5 +10,13 @@ Route::prefix('/admin')->group(function () {
             ->name('admin.register.create');
         Route::post('/register', [RegisteredAdminController::class, 'store'])
             ->name('admin.register.store');
+        Route::get('/login', [AuthenticatedAdminSessionController::class, 'create'])
+            ->name('admin.login.create');
+        Route::post('/login', [AuthenticatedAdminSessionController::class, 'store'])
+            ->name('admin.login.store');
+    });
+    Route::middleware('admin')->group(function () {
+        Route::post('/logout', [AuthenticatedAdminSessionController::class, 'destroy'])
+            ->name('admin.logout');
     });
 });
