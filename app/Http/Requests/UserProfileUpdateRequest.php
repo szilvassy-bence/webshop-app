cc6904819eaf10d10cda88b21962cec458c5706d
Szilvássy Bence <benceszilvassy91@gmail.com>
2024-07-09 12:47:53 +0200

    created UserProfileUpdateRequest.php

---
+++ app/Http/Requests/UserProfileUpdateRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UserProfileUpdateRequest extends FormRequest
+{
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $userId = $this->route('user');
+
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($userId)],
+        ];
+    }
+}
